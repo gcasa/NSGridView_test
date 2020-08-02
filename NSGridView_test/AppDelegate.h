d6f2906698251d477dda37c97ef0dbd2db6d2178
Gregory Casamento <greg.casamento@gmail.com>
2020-08-02 13:54:29 -0400

    Initial Commit

---
+++ NSGridView_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSGridView_test
+//
+//  Created by Gregory Casamento on 8/2/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
