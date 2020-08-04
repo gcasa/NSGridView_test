52721d70bcb9a49b8b22bf01aaeca6381953c27c
Gregory Casamento <greg.casamento@gmail.com>
2020-08-04 16:30:14 -0400

    Add GridView

--- NSGridView_test/AppDelegate.m
+++ NSGridView_test/AppDelegate.m
@@ -10,13 +10,32 @@
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property (retain) IBOutlet NSWindow *window;
+@property (retain) IBOutlet NSGridView *gridView;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    // NSMutableArray *array = [NSMutableArray arrayWithCapacity: 32];
+    NSUInteger i = 0;
+    for (i = 0; i < 32; i++)
+    {
+        float rand_max = RAND_MAX;
+        float red = rand() / rand_max;
+        float green = rand() / rand_max;
+        float blue = rand() / rand_max;
+        // NSColor* myColor = [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:1.0];
+        
+        NSView *v = [[NSView alloc] init];
+        NSTextField *tv = [[NSTextField alloc] init];
+        [tv setStringValue:@"Test"];
+        [v addSubview: tv];
+        
+        [self.gridView addSubview: v];
+    }
 }
 
 
