dd23e6b5390a944141d85dc4ac383ed997b7490c
Gregory Casamento <greg.casamento@gmail.com>
2021-03-22 18:25:09 -0400

    Add test for gridViewWithViews:

--- NSGridView_test/AppDelegate.m
+++ NSGridView_test/AppDelegate.m
@@ -13,30 +13,13 @@
 @property (retain) IBOutlet NSWindow *window;
 @property (retain) IBOutlet NSGridView *gridView;
 
+@property (retain) IBOutlet NSWindow *window1;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-    // NSMutableArray *array = [NSMutableArray arrayWithCapacity: 32];
-    /*
-    NSUInteger i = 0;
-    for (i = 0; i < 32; i++)
-    {
-        float rand_max = RAND_MAX;
-        float red = rand() / rand_max;
-        float green = rand() / rand_max;
-        float blue = rand() / rand_max;
-        // NSColor* myColor = [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:1.0];
-        
-        NSView *v = [[NSView alloc] init];
-        NSTextField *tv = [[NSTextField alloc] init];
-        [tv setStringValue:@"Test"];
-        [v addSubview: tv];
-        
-        [self.gridView addSubview: v];
-    }*/
     NSInteger r = 1;
     NSRect f = NSMakeRect(0, 0,500, 400);
     NSButton *v = [[NSButton alloc] initWithFrame: f];
@@ -50,6 +33,12 @@
     NSLog(@"NSGridView has %ld subviews.", [[self.gridView subviews] count]);
     NSLog(@"NSGridViewSizeForContent = %f", NSGridViewSizeForContent);
     NSLog(@"Nunber of cells in row %ld = %ld",r, [[self.gridView columnAtIndex:r] numberOfCells]);
+    
+    
+    NSArray *views = @[@[[[NSButton alloc] init],[[NSColorWell alloc] init]],@[[[NSButton alloc] init],[[NSColorWell alloc] init]]];
+    NSGridView *gv = [NSGridView gridViewWithViews:views];
+    [gv setFrame: NSMakeRect(50,50,200,200)];
+    [[self.window1 contentView] addSubview: gv];
 }
 
 
